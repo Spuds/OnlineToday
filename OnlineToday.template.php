c30555d11f5a864c7e67c272ed6c7e758a4e18bd
emanuele <emanuele45@gmail.com>
2014-09-21 16:01:31 +0200

    Updated version and fixed package-info

--- OnlineToday.template.php
+++ OnlineToday.template.php
@@ -17,7 +17,7 @@
  * copyright:	2011 Simple Machines (http://www.simplemachines.org)
  * license:  	BSD, See included LICENSE.TXT for terms and conditions.
  *
- * @version 0.1
+ * @version 0.1.1
  *
  */
 
